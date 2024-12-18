416e7ce6d566a6dace8b5552666a89258b483fcf
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-18 17:24:06 +0300

    Added extra fields, i.e, location and birth_date

--- profiles/models.py
+++ profiles/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-from users.models import CustomUser   # Ensure this imports your CustomUser  model
+from users.models import CustomUser    # Ensure this imports your CustomUser  model
 
 class Profile(models.Model):
-    user = models.OneToOneField(CustomUser , on_delete=models.CASCADE)  # Use CustomUser  here
-    bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)
+    user = models.OneToOneField(CustomUser , on_delete=models.CASCADE)  # Link to CustomUser 
+    bio = models.TextField(blank=True)  # Short biography
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)  # Profile picture
+    location = models.CharField(max_length=100, blank=True)  # User's location
+    birth_date = models.DateField(null=True, blank=True)  # User's birth date
 
     def __str__(self):
         return self.user.username
