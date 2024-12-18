f795930735b9e6812edaded728237210afdf23a8
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-18 17:07:32 +0300

    Simplify Profile model to include bio, profile_picture, location, and birth_date fields

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group, Permission 
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
 
 # Custom user model extending AbstractUser
 class CustomUser (AbstractUser ):
