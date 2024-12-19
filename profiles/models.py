b862b0a2a24326dc311343e0c2cb5c598c454e89
DavidMarkNjogu <davidmarknjogu@gmail.com>
2024-12-19 12:03:37 +0300

    `Revert back to using AUTH_USER_MODEL instead of CustomUser in Profile model`

--- profiles/models.py
+++ profiles/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from users.models import CustomUser    # Ensure this imports your CustomUser  model
 
 class Profile(models.Model):
-    #user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    user = models.OneToOneField(CustomUser , on_delete=models.CASCADE)  # Link to CustomUser 
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    #user = models.OneToOneField(CustomUser , on_delete=models.CASCADE)  # Link to CustomUser 
     bio = models.TextField(blank=True)  # Short biography
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True)  # Profile picture
     location = models.CharField(max_length=100, blank=True)  # User's location
